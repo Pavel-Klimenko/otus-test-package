3395b9835ff3347a922b59b59fc64d5284026245
Pavel Klimenko <p.klimenko@itachsoft.com>
2025-02-14 17:11:26 +0300

    Создан пакет composer

---
+++ src/Functions/TestFunctionalClass.php
@@ -0,0 +1,11 @@
+<?php declare(strict_types=1);
+
+namespace Pavelklimenko\OtusTestPackage\Functions;
+
+class TestFunctionalClass
+{
+    public function calculateSum(int $a, int $b): int
+    {
+        return $a + $b;
+    }
+}
